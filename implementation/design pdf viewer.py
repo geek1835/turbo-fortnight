5b6ac86f841dcf2aa4a7867ada328e2b7870b65e
pkr12 <44521021+pkr12@users.noreply.github.com>
2018-10-27 14:55:39 +0530

    create design pdf viewer.py

---
+++ implementation/design pdf viewer.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the designerPdfViewer function below.
+def designerPdfViewer(h, word):
+    
+    maxH=0
+    for i in range(len(word)):
+        if(h[ord(word[i])-97] > maxH):
+            maxH=h[ord(word[i])-97]
+    return(len(word)*1*maxH)
+    
+    
+
+     
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
